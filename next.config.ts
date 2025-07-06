b94bee6d5ec8eb091a9d3e6ae7997cbf0a635260
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-07 01:06:52 +0200

    add configuration for deploy on github server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/portfolio-app',
+
+  assetPrefix: '/portfolio-app/',
+
+  output: 'export',
+  
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
