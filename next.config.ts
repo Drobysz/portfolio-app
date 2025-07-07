29f30b10f553d5fd7726558d9ac3b172fe546cf4
Drobysz <alexanderdrobyschevski@gmail.com>
2025-07-07 02:29:05 +0200

    Delete /docs

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/portfolio-app',
-
-  assetPrefix: '/portfolio-app/',
-
-  output: 'export',
-  
-  productionBrowserSourceMaps: false,
+  // 
 };
 
 export default nextConfig;
